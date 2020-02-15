8bf169d5bd509d8a813b7a251d49108278a75c8c
d4tAloUh <d4tAloUh@users.noreply.github.com>
2020-02-15 10:50:34 +0200

    new changes

--- src/Test.java
+++ src/Test.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-        SearchQuery sq = new SearchQuery("docs\\");
-        System.out.println(sq.search("dog"));
-        System.out.println(sq.search("you"));
-        System.out.println("Use ( , ) , | , \\, & with spaces between");
-        System.out.println(sq.search("cat"));
         System.out.println(sq.search(" cat \\  dog  & you "));
+        SearchQuery sq = new SearchQuery("docs\\");
+        System.out.println(sq.search("you"));
+        System.out.println(sq.search("dog"));
+        System.out.println(sq.search("cat"));
+        System.out.println("Use ( , ) , | , \\, & with spaces between");
         Scanner sc = new Scanner(System.in);
         System.out.println(sq.search(sc.nextLine()));
     }
