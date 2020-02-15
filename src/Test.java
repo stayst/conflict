7fc24882d23bed90999d961a68595e8c28651f71
kate <kate.lisov@gmail.com>
2020-02-15 10:51:03 +0200

    Swap lines

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,8 @@ public class Test {
     public static void main(String[] args) {
         SearchQuery sq = new SearchQuery("docs\\");
         System.out.println(sq.search("dog"));
-        System.out.println(sq.search("cat"));
         System.out.println(sq.search("you"));
+        System.out.println(sq.search("cat"));
         System.out.println("Use ( , ) , | , \\, & with spaces between");
         System.out.println(sq.search("cat"));
         System.out.println(sq.search(" cat \\  dog  & you "));
